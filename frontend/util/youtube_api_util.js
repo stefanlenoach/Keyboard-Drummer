5698c20c832c98aa4e0e6b357653e4d78b4b67f5
Stefan Le Noach <stefanlenoach@gmail.com>
2016-06-30 13:52:27 -0400

    trying to get master up to date

---
+++ frontend/util/youtube_api_util.js
@@ -0,0 +1,39 @@
+module.exports = {
+  loadIframePlayer: function () {
+    var tag = document.createElement('script');
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+  },
+
+  createIframe: function (youtubeId, onPlayerStateChange) {
+    // if YT is not yet defined, return false to redirect
+    // to TrackList
+    if (typeof YT === "undefined") {
+      return false;
+    } else {
+      return new YT.Player('song-container', {
+        videoId: youtubeId,
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        wmode: "transparent",
+        playerVars: {
+          'autoplay': 0,
+          'controls': 0,
+          modestBranding: 1,
+          showinfo: 0,
+          fs: 0,
+          disablekb: 0,
+          rel:0,
+          iv_load_policy: 3
+        },
+        events: {
+          'onStateChange': onPlayerStateChange
+        }
+      });
+    }
+  }
+};
